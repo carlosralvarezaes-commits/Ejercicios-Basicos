c9a63ee3f2523c847ce70256be6eba67eb533bd7
carlosraul <carlosralvareza.es@gmail.com>
2025-10-21 18:27:17 +0200

    actua

---
+++ src/PUERTAS.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class PUERTAS {
+    public static void main(String[] args) {
+
+
+        System.out.println("Elige las puertas:");
+        System.out .println("¿1,2 o 3?");
+        Scanner eleccion = new Scanner(System.in);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
